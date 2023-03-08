bfacf96ff16c97c52f03ec01594013f04e4d4a59
narantyo maulana <olandeoxys@gmail.com>
2023-03-08 12:24:51 +0700

    [Add] deploy heroku

--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ var seeder = require('mongoose-seed');
 var mongoose = require('mongoose');
 
 // Connect to MongoDB via Mongoose
-seeder.connect('mongodb://127.0.0.1:27017/db_travelkuy', {
+seeder.connect('mongodb+srv://codeathome:bwamern@cluster0-40j6e.mongodb.net/db_staycation?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: true,
